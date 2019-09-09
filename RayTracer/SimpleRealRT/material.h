a9115d40341d27a61dace0d5c63bc303a81993dc
joseph-zhang <zyt5002@126.com>
2019-09-09 17:33:37 +0800

    add volumes

--- RayTracer/SimpleRealRT/material.h
+++ RayTracer/SimpleRealRT/material.h
@@ -152,4 +152,18 @@ class diffuse_light : public material {
   texture* emit;
 };
 
+class isotropic : public material {
+ public:
+  isotropic(texture* a) : albedo(a) {}
+  virtual bool scatter(const ray& r_in, const hit_record& rec,
+                       vec3& attenuation, ray& scattered) const {
+    // isotropic scatter
+    scattered = ray(rec.p, random_on_unit_sphere());
+    attenuation = albedo->value(rec.u, rec.v, rec.p);
+    return true;
+  }
+
+  texture* albedo;
+};
+
 #endif
