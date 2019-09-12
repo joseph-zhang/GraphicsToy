a17d519566583c213b46eed78e0bf80fe57331a4
joseph-zhang <zyt5002@126.com>
2019-09-12 16:17:21 +0800

    update vec3

--- RayTracer/Basic/vec3.h
+++ RayTracer/Basic/vec3.h
@@ -82,6 +82,10 @@ inline vec3 operator*(float t, const vec3 &v) {
   return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
+inline vec3 operator*(const vec3 &v, float t) {
+  return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
 inline vec3 operator/(const vec3 &v, float t) {
   return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
 }
